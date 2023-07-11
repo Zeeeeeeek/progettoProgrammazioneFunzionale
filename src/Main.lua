14c75ed6544b451c032f1368acaff616802d7c9f
Enrico Ulissi <enricoulissi01@gmail.com>
2023-07-11 10:23:06 +0200

    run function and config validation

--- src/Main.lua
+++ src/Main.lua
@@ -1,9 +1,9 @@
 N = 10
 D = {
-    S = { { 8, 3, 0, 0, 0, 0 }, { 3, 4, 0, 0, 0, 0 }, { 4, 1, 0, 0, 0, 0 }, { 3, 2, 0, 0, 0, 0 } },
+    S = { { 1, 3, 0, 0, 0, 0 }, { 3, 4, 0, 0, 0, 0 }, { 4, 1, 0, 0, 0, 0 }, { 3, 2, 0, 0, 0, 0 } },
     U = { { 8, 4 }, { 1, 1 } },
     C = { { 3, 8 } },
-    G = { { 4, 3 } },
+    G = { { 4, 8 } },
     R = { { 8, 7 }, { 6, 1 }, { 4, 5 } }
 }
 NoObjects = {
@@ -27,10 +27,27 @@ require("Move")
 require("Objects")
 
 ----Play the game until the config is final.
-function play(config)
-    printConfig(config)
-    print("--------------------------------------------------")
-    return isFinalConfig(config) and config or play(gong(config))
+function run(config, N)
+    if not isValidConfig(config, N) then
+        print("Invalid config")
+        return config
+    end
+    local function play(conf)
+        printConfig(conf)
+        print("--------------------------------------------------")
+        return isFinalConfig(conf) and conf or play(gong(conf))
+    end
+    return play(config)
+end
+
+function isValidConfig(config, N)
+    local function isOutOfBounds(position)
+        return (position[1] < 0 or position[1] >= N) or (position[2] < 0 or position[2] >= N)
+    end
+    return #config["S"] > 0 and (#filter(map(function(e)
+        return { e[1], e[2] }
+    end, config["S"]), isOutOfBounds) +
+            #filter(tableOfAllObjectsPositions(config), isOutOfBounds)) <= 0
 end
 
 ----A config is final if there is only one Senpai left and there are no objects left.
@@ -46,7 +63,7 @@ function gong(config)
 end
 
 function printConfig(config)
-    local keys = {"S", "U", "C", "G", "R"}
+    local keys = { "S", "U", "C", "G", "R" }
     for _, key in ipairs(keys) do
         io.write(key .. " = { ")
         for j, value in ipairs(config[key]) do
@@ -63,6 +80,8 @@ function printConfig(config)
     end
 end
 
-for _, c in ipairs(configs) do
-    play(c)
-end
+--for _, c in ipairs(configs) do
+--    run(c)
+--end
+
+run(D, N)
